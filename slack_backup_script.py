6c5081eb9517be8129fbdb3f98be995b2e147f8a
shotahirama <vghjjj@gmail.com>
2018-02-12 03:01:10 +0900

    fix error and add day interval

--- slack_backup_script.py
+++ slack_backup_script.py
@@ -13,20 +13,22 @@ from slackclient import SlackClient
 from datetime import datetime
 from six.moves import input
 import os
+from tqdm import tqdm
 
 
 class SlackBackup(object):
-    def __init__(self, conf,backup_dirname="backup"):
+    def __init__(self, conf, backup_dirname="backup",day_interval=10):
         self.set_login(conf)
         self.get_token_and_name()
         self.make_backup_directory(backup_dirname)
         self.sc = SlackClient(self.token)
+        self.run(day_interval=day_interval)
 
     def make_backup_directory(self, backup_dirname):
         self.downloads_dir = backup_dirname + "_" + datetime.now().strftime("%Y%m%d_%H%M%S")
         if not os.path.exists(self.downloads_dir):
             os.makedirs(self.downloads_dir)
-        print("Backup Directory:",self.downloads_dir)
+        print("Backup Directory:", self.downloads_dir)
 
     def set_login(self, conf):
         self.teamname = conf["teamname"] if "teamname" in conf else input("TeamName: ")
@@ -58,39 +60,62 @@ class SlackBackup(object):
             exit(1)
 
     def download_file(self, downfile):
-        filedata = requests.get(downfile["url_private_download"], headers={"Authorization": "Bearer %s" % self.token},
-                                stream=True)
-        print("Downloading ...", downfile["name"])
-        with open(os.path.join(self.downloads_dir,
-                               datetime.fromtimestamp(int(downfile["timestamp"])).strftime("%Y%m%d_%H%M%S") + "_" +
-                                       downfile["name"]), "wb") as f:
-            for chunk in filedata.iter_content(chunk_size=1024):
-                f.write(chunk)
+        try:
+            filedata = requests.get(downfile["url_private_download"], headers={"Authorization": "Bearer %s" % self.token},
+                                    stream=True)
+            print("Downloading ...", downfile["name"])
+            with open(os.path.join(self.downloads_dir,
+                                   datetime.fromtimestamp(int(downfile["timestamp"])).strftime("%Y%m%d_%H%M%S") + "_" +
+                                           downfile["name"]), "wb") as f:
+                for chunk in filedata.iter_content(chunk_size=1024):
+                    f.write(chunk)
+        except KeyError:
+            pass
 
-    def run(self):
+    def run(self,day_interval):
+        backup_delete_list = []
         fileslist = self.sc.api_call("files.list")
-        fls = fileslist["files"]
-        for fl in fls:
-            self.download_file(fl)
+        for fl in fileslist["files"]:
+            if (datetime.now() - datetime.fromtimestamp(fl["timestamp"])).days > day_interval:
+                backup_delete_list.append(fl)
+                # self.download_file(fl)
         maxpage = int(fileslist["paging"]["pages"])
-        count = len(fileslist["files"])
         for i in range(2, maxpage + 1):
-            ffl = self.sc.api_call("files.list", page=i)
-            for fl in ffl["files"]:
+            fileslist = self.sc.api_call("files.list", page=i)
+            for fl in fileslist["files"]:
+                if (datetime.now() - datetime.fromtimestamp(fl["timestamp"])).days > day_interval:
+                    backup_delete_list.append(fl)
+                    # self.download_file(fl)
+        cant_download_filelist = []
+        for fl in tqdm(backup_delete_list):
+            try:
+                private_url = fl["url_private_download"]
                 self.download_file(fl)
-            count += len(ffl["files"])
-        print(count)
+            except:
+                cant_download_filelist.append(fl)
+
+        self.get_channel()
+        for cdf in cant_download_filelist:
+            for ch in cdf["channels"]:
+                print(cdf["name"],self.channels_dict[ch])
+
+    def get_channel(self):
+        channels = self.sc.api_call("channels.list")["channels"]
+        self.channels_dict = {}
+        for channel in channels:
+            self.channels_dict[channel["id"]] = channel["name"]
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Slack Files Backup Script")
     parser.add_argument("--backupdir", "-b", action="store", default="backup")
     parser.add_argument("--config", "-c", action="store")
+    parser.add_argument("--day", "-d", action="store",type=int, default=10)
     args = parser.parse_args()
     if args.config:
         with open(args.config) as f:
             config = yaml.load(f)
     else:
         config = {}
-    slackbak = SlackBackup(config,backup_dirname=args.backupdir)
-    slackbak.run()
+    slackbak = SlackBackup(config, backup_dirname=args.backupdir,day_interval=args.day)
